6ccde86a2c5af4a1f6cfbec02b4fbca1c7fe64dc
denmaen2 <denmaen2@gmail.com>
2025-06-22 15:05:37 +0000

    moved files to Milestone1

--- sonar/requirements.sh
+++
@@ -1,4 +0,0 @@
-wget https://services.gradle.org/distributions/gradle-6.9.4-bin.zip -P /tmp
-unzip -d /tmp /tmp/gradle-6.9.4-bin.zip
-/tmp/gradle-6.9.4/bin/gradle wrapper --gradle-version 6.9.4
-chmod +x gradlew
